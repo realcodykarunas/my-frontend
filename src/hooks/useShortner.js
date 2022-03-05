71382e90aadda72593465def127764991690a38e
realcodykarunas <101003379+realcodykarunas@users.noreply.github.com>
2022-03-05 14:43:43 -0600

    Get the url and slug from form input

--- src/hooks/useShortner.js
+++ src/hooks/useShortner.js
@@ -1,11 +1,17 @@
-// import { useMutation } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+import { ADD_LINK } from "../mutations/mutations";
 
 export const useShortner = () => {
-  // const [addLink, { data, loading, error }] = useMutation();
+  const [addLink, { data, loading, error }] = useMutation(ADD_LINK);
 
   const handleSubmit = (value) => {
-    const { url } = value;
-    console.log(url);
+    const { url: urlValue } = value;
+    const res = urlValue.split("/");
+    const url = `${res[0]}//${res[1]}${res[2]}`;
+    const slug = res.slice(-2).join("/");
+    console.log("url :", url);
+    console.log("slug: ", slug);
+    // addLink({ variables: { url, slug } });
   };
 
   const handleError = (error) => {
